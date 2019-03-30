b861439787d9c39d801de84e6506d4a31e9260a7
DeadlyMC <modding.deadlymc@gmail.com>
2019-03-30 15:37:29 +0530

    fixed typo

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -144,7 +144,7 @@ public class CarpetSettings
   //                                         "Requires flying to be enabled on the server"),
   rule("explosionNoBlockDamage", "tnt", "Explosions won't destroy blocks"),
   rule("tntPrimerMomentumRemoved", "tnt", "Removes random TNT momentum when primed"),
-  rule("fastRedstoneDust",      "experimental optimizations", "Lag optimizations for rDedstone dust")
+  rule("fastRedstoneDust",      "experimental optimizations", "Lag optimizations for redstone dust")
                                 .extraInfo("by Theosib").boolAccelerate().defaultFalse(),
   //<with modified protocol> rule("accurateBlockPlacement", "creative", "Allows to place blocks in different orientations. Requires Carpet Client")
   //                              .extraInfo("Also prevents rotations upon placement of dispensers and furnaces","when placed into a world by commands"),
